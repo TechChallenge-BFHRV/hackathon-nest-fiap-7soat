571519ad605c16970f0d3818f230b0a54389272c
Vinicius de Liz <vinicius.deliz@gmail.com>
2025-01-09 16:35:43 -0300

    feat: setup swagger in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/v1');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+  );
+  const config = new DocumentBuilder()
+  .setTitle('Hackathon 7SOAT FIAP')
+  .setDescription('Grupo 49')
+  .setVersion('1.0')
+  .addBearerAuth()
+  .build();
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
