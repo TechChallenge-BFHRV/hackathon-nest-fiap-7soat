27dbc9a7a2d9faab1676454ec33790d6cfc1e7ea
Vinicius de Liz <vinicius.deliz@gmail.com>
2025-01-12 23:18:30 -0300

    feat: update video service test

--- src/video/video.service.spec.ts
+++ src/video/video.service.spec.ts
@@ -2,13 +2,14 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { VideoService } from './video.service';
 import { UploadstatusService } from '../uploadstatus/uploadstatus.service';
 import { PrismaService } from '../prisma/prisma.service';
+import { MessagesService } from '../messages/messages.service';
 
 describe('VideoService', () => {
   let service: VideoService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [VideoService, UploadstatusService, PrismaService],
+      providers: [VideoService, UploadstatusService, PrismaService, MessagesService],
     }).compile();
 
     service = module.get<VideoService>(VideoService);
